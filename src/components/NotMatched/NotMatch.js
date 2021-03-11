32ad7268f191ed2f34aa96ef89b317b5a4a93b65
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-03-11 21:25:25 +0600

    finishing touch

--- src/components/NotMatched/NotMatch.js
+++ src/components/NotMatched/NotMatch.js
@@ -4,7 +4,7 @@ const NotMatch = () => {
     return (
         <div className='text-center text-white pt-5 pb-5'>
             <h3>Sorry, page not found.</h3>
-            <h5>404 Error found !!!</h5>
+            <h5>404 Error found !</h5>
         </div>
     );
 };
